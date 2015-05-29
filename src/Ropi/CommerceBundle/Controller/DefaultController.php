2238916043c9bd1a4fae58a9235c4471a0ab28af
Laurent5 <contact@laurentcardon.be>
2015-05-29 03:45:31 +0200

    Gestion des commerçants, suite ...

--- src/Ropi/CommerceBundle/Controller/DefaultController.php
+++ src/Ropi/CommerceBundle/Controller/DefaultController.php
@@ -2,21 +2,25 @@
 
 namespace Ropi\CommerceBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use Ropi\CommerceBundle\Entity\Commerce;
+use Ropi\CommerceBundle\Form\CommerceType;
+use Ropi\IdentiteBundle\Entity\Adresse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Ropi\CommerceBundle\Form\CommerceType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Ropi\CommerceBundle\Entity\Commerce;
-use Ropi\IdentiteBundle\Entity\Adresse;
-use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class DefaultController extends Controller {
+
+    private $route = "admin_home"; //Route de redirection par défaut après une action
 
     /**
      * @Route("/my/commerce/new")
      * @Template()
      */
+
     public function newCommerceAction(Request $request) {
 
         $form = $this->createCommercantForm($request);
@@ -25,7 +29,7 @@ class DefaultController extends Controller {
             $em = $this->getDoctrine()->getManager();
 
             $commerce = $form->getData();
-//$commerce->addPersonne($this->getUser()->getPersonne());
+            //$commerce->addPersonne($this->getUser()->getPersonne());
             $adresses = $commerce->getAdresses();
 
             foreach ($adresses as $adresse) {
@@ -70,26 +74,24 @@ class DefaultController extends Controller {
             $commercant = $repo->findOneBy(array('id' => $id));
 
             if ($commercant) {
-                $commercant->setActive(true);
+                $commercant->setValide(true);
                 $this->getDoctrine()->getManager()->flush();
             }
 
             $this->get('session')->getFlashBag()->add('success', 'Le commerce ' . $commercant->getNom() . ' a bien été validé');
-
-            $this->redirect($this->generateUrl("commerce_validate"));
-        } else {
-            return array(
-                'commerces' => $repo->findBy(array('visible' => false)),
-            );
         }
+
+        return array(
+            'commerces' => $repo->findBy(array('valide' => false)),
+        );
     }
 
     /**
-     * @Route("/my/commerce/update/{id}", name="commerce_update")
+     * @Route("/my/commerce/update/{id}/{route}", requirements={"id": "\d+"}, defaults={"route": null}, name="commerce_update")
      * @Secure("ROLE_ADMIN")
      * @Template()
      */
-    public function updateAction(Request $request, $id) {
+    public function updateAction(Request $request, $id, $route = null) {
         $commerce = $this->getDoctrine()->getRepository("Ropi\CommerceBundle\Entity\Commerce")->find($id);
 
         if ($commerce) {
@@ -101,16 +103,90 @@ class DefaultController extends Controller {
 
                 $this->get('session')->getFlashBag()->add('success', 'Modifications prisent en compte');
 
-                $this->redirect($this->generateUrl("commerce_home"));
+                return $this->gestionRoute($route);
             }
 
             return $this->render("RopiCommerceBundle:Default:newCommerce.html.twig", array(
                         'form' => $form->createView(),
             ));
-            
         } else {
             throw $this->createNotFoundException();
         }
+    }
+
+    /**
+     * @Secure(roles={"ROLE_ADMIN"})
+     * 
+     * @Route("/my/commerce/remove/{id}/{route}", requirements={"id": "\d+"}, defaults={"route": null}, name="commerce_remove")
+     * 
+     */
+    public function removeAction($id, $route = null) {
+        $repo = $this->getDoctrine()->getRepository("Ropi\CommerceBundle\Entity\Commerce");
+        $commerce = $repo->find($id);
+        if ($commerce) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($commerce);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Le commerce a bien été supprimé');
+        }
+
+        return $this->gestionRoute($route);
+    }
+
+    /**
+     * @Secure(roles={"ROLE_ADMIN"})
+     * @Route("/my/admin/commerces", name="admin_commerces")
+     * @Template()
+     */
+    public function indexAction() {
+        $repo = $this->getDoctrine()->getRepository("Ropi\CommerceBundle\Entity\Commerce");
+        $commerces = $repo->findBy(array('valide' => true));
+
+        return array(
+            'commerces' => $commerces,
+            'route' => 'admin_commerces'
+        );
+    }
+
+    /**
+     * 
+     * @Route("/my/commerce/{proprety}/{id}/{route}", requirements={"id": "\d+"}, name="commerce_change")
+     * @Secure(roles={"ROLE_ADMIN"})
+     * 
+     */
+    public function changeProprety($proprety, $id, $route = null) {
+        $repo = $this->getDoctrine()->getRepository('Ropi\CommerceBundle\Entity\Commerce');
+        $em = $this->getDoctrine()->getManager();
+        $commerce = $repo->find($id);
+        if ($commerce) {
+            $proprety = ucfirst($proprety);
+            $get = "get" . $proprety;
+            $set = "set" . $proprety;
+            $commerce->$set(!$commerce->$get());
+            $em->flush();
+
+            $this->addFlash('success', 'La modification a bien été réalisée');
+        } else {
+            $this->addFlash('danger', 'Le modification a échouée');
+        }
+
+        return $this->gestionRoute($route);
+    }
+
+    private function gestionRoute($route) {
+        if (isset($route)) {
+
+            try {
+                $route = $this->generateUrl($route);
+            } catch (RouteNotFoundException $ex) {
+                $route = $this->generateUrl($this->route);
+            }
+        } else {
+            $route = $this->generateUrl($this->route);
+        }
+
+        return $this->redirect($route);
     }
 
 }
