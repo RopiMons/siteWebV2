6732272ed7920600061ddedc873395f9cb7fd7af
adrien huygens <adrien.huygens@gmail.com>
2017-04-24 15:43:00 +0200

    Correctin bug des news avec images, attende provisoire d'une direction d'affichage
    Signed-off-by: adrien huygens <adrien.huygens@gmail.com>

--- src/IolaCorporation/NewsBundle/Controller/NewsController.php
+++ src/IolaCorporation/NewsBundle/Controller/NewsController.php
@@ -17,7 +17,7 @@ class NewsController extends Controller
 {
     public function indexAction()
     {
-        $news =  $this->getDoctrine()->getRepository(News::class)->findNewNews(1,0);
+        $news =  $this->getDoctrine()->getRepository(News::class)->findNewNews(3,0);
         return $this->render('IolaCorporationNewsBundle:Default:news.html.twig',array('news'=>$news));
     }
 
